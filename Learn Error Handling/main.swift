b8e0aa1cc432227f09b6d6dd19130edf8dfdf776
Steven Hertz <menashehertz@gmail.com>
2017-12-12 16:57:15 -0500

    Initial Commit

---
+++ Learn Error Handling/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Learn Error Handling
+//
+//  Created by Steven Hertz on 12/12/17.
+//  Copyright © 2017 Steven Hertz. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
